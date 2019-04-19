1fbef22afd3fde239d58f52ed5d00fb2fdc2eb94
djndl1 <djndl1@gmail.com>
2019-04-19 17:38:46 +0800

    pwd

---
+++ src/date.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import sys
+import datetime
+
+def date(fmt):
+    
---
+++ src/pwd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+def pwd(args):
+    pwd_logical = os.getenv('PWD')
+    pwd_physical = os.getcwd()
+
+    args_r = reversed(args)
+    for arg in args_r:
+        if arg == '-P':
+            return pwd_physical
+        
+    return pwd_logical
+
+if __name__ == '__main__':
+    pwd = pwd(sys.argv)
+    print(pwd)

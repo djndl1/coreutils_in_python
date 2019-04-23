6739b8ef794540d90626c1069154697d7af9df07
djndl1 <djndl1@gmail.com>
2019-04-23 17:45:07 +0800

    another day of idleness

--- src/date.py
+++ src/date.py
@@ -2,8 +2,24 @@
 # -*- coding:utf-8 -*-
 
 import sys
-import datetime
+import os
+from datetime import datetime, date, time
+import time
+# from dateutil import *
 
 def date(UTC, fmt):
-    lang_env = 
+    lc_time_env = os.getenv('LC_TIME')
+    tz_env = os.getenv('TZ')
+
+    if UTC:
+        tz = 'UTC0'
+        utc_offset = timedelta(0)
+        daylight = 0
+    elif tz_env:
+        tz = tz_env
+        time.tzset()
+        utc_offset =  
+
+
+
     

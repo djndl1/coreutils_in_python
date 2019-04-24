12b3fc6ab2f9302df5d99c0f2afaeb96136951ce
djndl1 <djndl1@gmail.com>
2019-04-24 17:45:21 +0800

    date nearly finished

--- src/date.py
+++ src/date.py
@@ -1,25 +1,53 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
-import sys
-import os
-from datetime import datetime, date, time
-import time
-# from dateutil import *
+# For POSIX standard on this utility,
+# see http://pubs.opengroup.org/onlinepubs/9699919799.2018edition/utilities/date.html
 
-def date(UTC, fmt):
+import sys, os
+import argparse
+import time
+from datetime import datetime, timedelta, timezone
+
+
+def get_cur_date(UTC=False):
     lc_time_env = os.getenv('LC_TIME')
     tz_env = os.getenv('TZ')
 
     if UTC:
-        tz = 'UTC0'
+        tz_name = 'UTC'
         utc_offset = timedelta(0)
-        daylight = 0
+        daylight = False
     elif tz_env:
-        tz = tz_env
         time.tzset()
-        utc_offset =  
+        utc_offset = timedelta(seconds=-time.altzone)
+        daylight = time.daylight
+        if daylight:
+            tz_name = time.tzname[1]
+        else:
+            tz_name = time.tzname[0]
+    else:
+        time.tzset()
+        utc_offset = timedelta(seconds=-time.altzone)
+        daylight = time.daylight
+        if daylight:
+            tz_name = time.tzname[1]
+        else:
+            tz_name = time.tzname[0]
+
+    tz = timezone(utc_offset, name=tz_name)
+    current_datetime = datetime.now(tz)
+
+    return current_datetime
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=
+        'Display the current time in the given FORMAT, or set the system date.')
+    parser.add_argument('format', action='store', type=str,
+                        default='+%a %b %e %H:%M:%S %Z %Y')
+    parser.add_argument('-u', action='store_false')
+    args = parser.parse_args()
+    print(get_cur_date(args.u).strftime(args.format[1:]))
 
     
